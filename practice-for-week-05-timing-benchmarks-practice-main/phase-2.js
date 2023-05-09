b1cfaceb5ee718ecb52f59b5472a71e702653c77
Cameron Norfleet <cameron@Camerons-MBP.lan>
2023-05-09 16:46:31 -0700

    p2

--- practice-for-week-05-timing-benchmarks-practice-main/phase-2.js
+++ practice-for-week-05-timing-benchmarks-practice-main/phase-2.js
@@ -2,13 +2,27 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
+  const finalArr = []
+
+  for(let i = 1; i <= 10; i++){
+    newVal = i * increment
+    finalArr.push(addNums(newVal))
+  }
+  return finalArr
 
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
+  finalArr = []
+
+  const start = increment
+  const end = increment * 10
+
+  for(let i = start; i <= end; i += increment){
+    finalArr.push(addManyNums(i))
+  }
+  return finalArr
 
 }
 
